254d7cc948fe621c371edd2ef9c6566dd1228bad
Casey Marshall <casey.marshall@canonical.com>
2016-04-04 22:55:48 -0500

    Initial commit

---
+++ reactive/mattermost.py
@@ -0,0 +1,53 @@
+import os
+import shutil
+
+from charms.reactive import hook, when, when_not, set_state, remove_state, is_state
+from charmhelpers.core import hookenv
+from charmhelpers.core.host import add_group, adduser, service_running, service_start, service_restart
+from charmhelpers.core.templating import render
+from charmhelpers.fetch import archiveurl, apt_install, apt_update
+from charmhelpers.payload.archive import extract_tarfile
+
+
+INSTALL_URL="https://github.com/mattermost/platform/releases/download/v2.1.0/mattermost.tar.gz"
+
+
+@hook('install')
+def install():
+    handler = archiveurl.ArchiveUrlFetchHandler()
+    handler.download(INSTALL_URL % (version), dest='/opt')
+
+    extract_tarfile('/srv/mattermost.tar.gz', destpath="/opt")
+ 
+    # Create mattermost user & group
+    add_group("mattermost")
+    adduser("mattermost", system_user=True)
+    
+    os.makedirs("/opt/mattermost/data", 0700)
+    shutil.chown("/opt/mattermost/data", user="mattermost", group="mattermost")
+
+    render(source='upstart',
+        target="/etc/init/mattermost.conf",
+        perms=0o644,
+        context={})
+
+
+@when("db.database.available")
+def setup(db):
+    conf = hookenv.config()
+    render(source='config.json',
+        target="/opt/mattermost/config/config.json",
+        perms=0o644,
+        context={
+            'conf': conf,
+            'db': db,
+        })
+    remove_state("db.database.available")
+    restart_service()
+
+
+def restart_service():
+    if service_running("mattermost")
+        service_restart("mattermost")
+    else:
+        service_start("mattermost")
